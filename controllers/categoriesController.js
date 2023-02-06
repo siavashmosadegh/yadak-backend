c3ceb75cf6aa917586c3686fcf37935a8fd90c8c
siavashmosadegh <63389544+siavashmosadegh@users.noreply.github.com>
2023-02-06 18:07:59 +0330

    Advanded Filtering => implemented gte , gt , lte , lt operations

--- controllers/categoriesController.js
+++ controllers/categoriesController.js
@@ -22,13 +22,20 @@ exports.createCategoryItem = async (req, res) => {
 
 exports.getCategories = async (req, res) => {
     try {
-        // console.log(req.query);
-        // BUILD QUERY 
+        // BUILD QUERY
+        // 1) Filtering
         const queryObj = { ...req.query };
-        const excludeFields = ['page', 'sort', 'limit', 'fields','duration'];
+        const excludeFields = ['page', 'sort', 'limit', 'fields'];
         excludeFields.forEach(el => delete queryObj[el]);
 
+        // 2) Advanced Filtering
+        let queryStr = JSON.stringify(queryObj);
+        queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
+        console.log(JSON.parse(queryStr));
+
         const query = CategoryItem.find(queryObj);
+
+        console.log(JSON.parse(queryStr));
 
         // EXECUTE QUERY
         const categories = await query;
